a2d849a65ad0fda091a2ca78ab8c64d9c38c1fff
jcmsj <sanjuan.jeancarlo@gmail.com>
2025-03-31 15:31:43 +0800

    add trust proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->appendToGroup('web', [
             \Spatie\MailPreview\Http\Middleware\AddMailPreviewOverlayToResponse::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
